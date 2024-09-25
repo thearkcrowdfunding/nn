8c46a1e751941ec40804f9eb80039fbde8d12de4
bkantorovich <bkantreg@gmail.com>
2024-09-25 18:14:07 +0400

    Empty commit

--- src/components/donation-form/subscription-options.ts
+++
@@ -1,5 +0,0 @@
-export const subscriptionOptions = [
-	{ value: "monthly-10", label: "$10", amount: 1000 },
-	{ value: "monthly-20", label: "$20", amount: 2000 },
-	{ value: "monthly-50", label: "$50", amount: 5000 },
-  ]
