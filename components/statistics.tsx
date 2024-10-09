d11c5c5516f6df86d4bac2f3ca42ffbe0136a6bd
bkantorovich <bkantreg@gmail.com>
2024-10-09 16:42:15 +0400

    Minor fixes

--- components/statistics.tsx
+++ components/statistics.tsx
@@ -3,7 +3,7 @@
 export function StatisticsComponent() {
   return (
     <div className="bg-white p-6 md:p-10 rounded-lg max-w-4xl mx-auto">
-      <h2 className="text-3xl md:text-4xl font-bold mb-8">НАСИЛИЮ.НЕТ – ЭТО</h2>
+      <h2 className="text-3xl md:text-4xl font-bold mb-8">НАСИЛИЮ.НЕТ — ЭТО</h2>
       
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="flex flex-col">
