080f3b86d260223a66610041cf2626556b974219
bkantorovich <bkantreg@gmail.com>
2024-10-09 16:15:13 +0400

    Minor fixes

--- components/hero.tsx
+++ components/hero.tsx
@@ -29,23 +29,23 @@ export function HeroComponent() {
         priority
       />
       <div className="absolute inset-0 bg-black opacity-60"></div>
-      <div className="relative z-10 max-w-[800px] w-full">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-left leading-tight">
+      <div className="relative z-10 max-w-[900px] w-full">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6 md:mb-12 text-left leading-tight">
           ПОМОГИТЕ ЖЕНЩИНАМ, СТРАДАЮЩИМ ОТ{"\u00A0"}ДОМАШНЕГО НАСИЛИЯ
         </h1>
         <p className="text-base md:text-lg mb-8 text-left">
           Получить <strong className="font-extrabold">экстренное убежище,</strong> <strong className="font-extrabold">психологическую помощь,</strong> <strong className="font-extrabold">комплексное сопровождение</strong> и{"\u00A0"}вырваться из{"\u00A0"}замкнутого круга
         </p>
-        <div className="flex flex-col md:flex-row md:items-center">
+        <div className="flex flex-col md:flex-row md:items-center md:justify-start">
           <button 
-            className="w-full md:w-auto md:flex-grow bg-red-500 hover:bg-red-600 text-white font-bold py-3 md:py-4 rounded-full transition duration-300 text-lg md:text-xl"
+            className="w-full md:w-[400px] bg-red-500 hover:bg-red-600 text-white font-bold py-3 md:py-4 px-6 rounded-full transition duration-300 text-lg md:text-xl"
             onClick={handleDonateClick}
           >
             <span className="block text-center">Помочь сейчас!</span>
           </button>
           <Link 
             href="#learn-more" 
-            className="text-base md:text-lg underline mt-4 md:mt-0 md:ml-6 text-center md:text-left whitespace-nowrap" 
+            className="text-base md:text-lg underline mt-4 md:mt-0 md:ml-12 text-center md:text-left whitespace-nowrap" 
             onClick={handleLearnMoreClick}
           >
             Узнать больше
